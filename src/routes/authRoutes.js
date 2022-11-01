2ecd70f8ec4ff9695185a54e63a7842f52792f57
marcelosrc <marcelosrc@gmail.com>
2022-10-31 21:52:58 -0300

    Reparo em app e authRoutes após commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const controller = require("../controllers/authControler");
 
-router.post("/auth", controller.login)
 router.post("/login", controller.login)
 
 module.exports = router;
